d8297a2bad1c4933bd2963a7537989f49e912eef
SANJIB KUMAR DAS <81639503+sanjib-kumar-das@users.noreply.github.com>
2022-02-23 12:04:31 +0530

    Add files via upload

---
+++ build/precache-manifest.0da9f85a18dac5987fea91c85a9eca22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49230dc3af5bd97c3a3abcabbee42d86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f36f7331282167b79f71",
+    "url": "/static/css/main.3fcd11d6.chunk.css"
+  },
+  {
+    "revision": "e9a0060c79171aacae58",
+    "url": "/static/js/2.0c19cf2e.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "f36f7331282167b79f71",
+    "url": "/static/js/main.f8d5135f.chunk.js"
+  },
+  {
+    "revision": "bb1f08fe37d3c65590eb",
+    "url": "/static/js/runtime~main.e015284a.js"
+  }
+]);
